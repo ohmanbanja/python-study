03ced253537da001a0053e315b5dbd59a46c0fc2
ohmanbanja <krlcks071200@naver.com>
2022-09-24 16:40:24 +0900

    2022-09-24 pyautogui

---
+++ dd.py
@@ -0,0 +1,17 @@
+import pyautogui
+
+
+#print("권기찬")
+#name = input()
+#print(name)
+
+#a = int(input())
+#b = int(input())
+
+#print(a+b)
+
+#def hello():
+ #   print("hello")
+#hello()
+#hello()
+
---
+++ main.py
@@ -0,0 +1,6 @@
+import pyautogui
+
+#print(pyautogui.position())
+
+#x=766 y=237
+pyautogui.moveTo(742,234,3)
